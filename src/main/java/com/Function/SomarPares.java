bb3c02dc6abc99990debe7398e155cef77f3ebfb
Eduardo <eduardofreitasfr@gmail.com>
2024-11-14 21:46:43 -0300

    Resolvido problema de somar

--- src/main/java/com/Function/SomarPares.java
+++ src/main/java/com/Function/SomarPares.java
@@ -2,13 +2,17 @@ package com.Function;
 
 import com.implementation.SomarParesInterface;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SomarPares implements SomarParesInterface {
     @Override
     public List<Integer> somarListaPares(List<Integer> lista){
-        return lista.stream()
-                .filter(numero -> numero % 2 == 0).collect(Collectors.toList()); // Coleta os números pares em uma nova lista
+        return Collections.singletonList(lista.stream()
+                .filter(numero -> numero % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum());
     }
+
 }
