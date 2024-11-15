c7e59c0499b450ebfa93e566d2b45067b310ddc6
Eduardo <eduardofreitasfr@gmail.com>
2024-11-14 21:45:40 -0300

    Novos implements

--- src/main/java/com/extend/MetadosExtends.java
+++ src/main/java/com/extend/MetadosExtends.java
@@ -1,14 +1,14 @@
 package com.extend;
 
-import com.implementation.ListaOrdemNumericaInterface;
-import com.implementation.SomarParesInterface;
-import com.implementation.VerificarNumerosPositivosInterface;
+import com.Function.CheckFunctionInterface;
+import com.implementation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
-//Desafio 2 - Imprima a soma dos números pares da lista:
-//Utilizando a Stream API, realize a soma dos números pares da lista e exiba o resultado no console.
-public class MetadosExtends implements ListaOrdemNumericaInterface, SomarParesInterface, VerificarNumerosPositivosInterface {
+
+public class MetadosExtends implements ListaOrdemNumericaInterface, SomarParesInterface, VerificarNumerosPositivosInterface , RemoverNumerosImparInterface, CheckFunctionInterface {
     @Override
     public List<Integer> ordenarListaOrdemCrescente(List<Integer> listaOrdenada) {
         return listaOrdenada.stream().sorted().collect(Collectors.toList());
@@ -16,12 +16,31 @@ public class MetadosExtends implements ListaOrdemNumericaInterface, SomarParesIn
 
     @Override
     public List<Integer> somarListaPares(List<Integer> lista) {
-        return lista.stream()
-                .filter(numero -> numero % 2 == 0).collect(Collectors.toList());
+        return Collections.singletonList(lista.stream()
+                .filter(numero -> numero % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum());
     }
 
     @Override
     public List<Integer> VerificarValoresPositivos(List<Integer> numeros) {
         return numeros.stream().filter(numero -> numero > 0).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Integer> DeletarNumerosImpar(List<Integer> listNumber) {
+        return listNumber.stream().filter(numero -> numero % 2 ==0).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Integer> DeletarNumerosPar(List<Integer> listNumber) {
+        return listNumber.stream().filter(numero -> numero % 2 != 0).collect(Collectors.toList());
+    }
+
+
+    @Override
+    public List<Integer> checkNumerosMaiorQueDez(List<Integer> list) {
+        return list.stream()
+                .filter(number -> number > 10).collect(Collectors.toList());
     }
 }
