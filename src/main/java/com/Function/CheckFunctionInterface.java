c85079411e2acc91672e8d3fc306592c9fde049b
Eduardo <eduardofreitasfr@gmail.com>
2024-11-14 21:46:15 -0300

    Adicionado interface

---
+++ src/main/java/com/Function/CheckFunctionInterface.java
@@ -0,0 +1,7 @@
+package com.Function;
+
+import java.util.List;
+
+public interface CheckFunctionInterface {
+    List<Integer> checkNumerosMaiorQueDez(List<Integer> list);
+}
---
+++ src/main/java/com/implementation/RemoverNumerosImparInterface.java
@@ -0,0 +1,9 @@
+package com.implementation;
+
+import java.util.List;
+public interface RemoverNumerosImparInterface {
+    //Desafio 4 - Remova todos os valores ímpares:
+//Utilize a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console.
+    List<Integer> DeletarNumerosImpar(List<Integer> listNumber);
+    List<Integer> DeletarNumerosPar(List<Integer> listNumber);
+}
