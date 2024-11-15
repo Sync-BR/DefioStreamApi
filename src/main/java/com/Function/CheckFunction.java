c9aab5297f2b04140944582ad20461a58f9404da
Eduardo <eduardofreitasfr@gmail.com>
2024-11-14 21:45:56 -0300

    Adicionado check de numeros maiores que 10

---
+++ src/main/java/com/Function/CheckFunction.java
@@ -0,0 +1,13 @@
+package com.Function;
+
+import java.util.List;
+import java.util.stream.Collectors;
+//Desafio 6 - Verificar se a lista contém algum número maior que 10:
+//Utilize a Stream API para verificar se a lista contém algum número maior que 10 e exiba o resultado no console.
+public class CheckFunction implements CheckFunctionInterface {
+    @Override
+    public List<Integer> checkNumerosMaiorQueDez(List<Integer> list){
+        return list.stream()
+                .filter(number -> number > 10).collect(Collectors.toList());
+    }
+}
